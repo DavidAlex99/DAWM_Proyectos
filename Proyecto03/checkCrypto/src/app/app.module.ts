55e91c68013a1457a35723c8156738581c26234b
DavidAlex99 <daalrive@espol.edu.ec>
2022-12-23 20:18:04 -0500

    addesd: component angular images

--- Proyecto03/checkCrypto/src/app/app.module.ts
+++ Proyecto03/checkCrypto/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GeneralComponent } from './components/general/general.component';
+import { BusquedaComponent } from './components/busqueda/busqueda.component';
+import { NoticiasComponent } from './components/noticias/noticias.component';
+import { EarnComponent } from './components/earn/earn.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GeneralComponent,
+    BusquedaComponent,
+    NoticiasComponent,
+    EarnComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
