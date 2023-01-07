5e9774be77e500735efb47a3041a4a3d0b5750a8
DavidAlex99 <daalrive@espol.edu.ec>
2023-01-07 18:06:56 -0500

    added: enrutamiento, seccion Earn

--- Proyecto03/cryptoCheck/src/app/app-routing.module.ts
+++ Proyecto03/cryptoCheck/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {CryptonewsComponent} from './components/cryptonews/cryptonews.component';
-import {HeadlinesComponent} from './components/headlines/headlines.component';
+import { CryptonewsComponent } from './components/cryptonews/cryptonews.component';
+import { HeadlinesComponent } from './components/headlines/headlines.component';
 import { GeneralComponent } from './components/general/general.component';
+import { BusquedaComponent } from './components/busqueda/busqueda.component';
+import { EarnComponent } from './components/earn/earn.component';
 
 const routes: Routes = [
-  {path:'', component: HeadlinesComponent},
-  {path:'technology', component: CryptonewsComponent},
-  { path: "general", component: GeneralComponent }
+  {path:'', component: GeneralComponent},
+  {path: 'cryptonews', component: HeadlinesComponent},
+  {path: 'general', component: GeneralComponent },
+  {path: 'earn', component: EarnComponent },
+  {path: 'busqueda', component: BusquedaComponent}
 ];
 
 @NgModule({
